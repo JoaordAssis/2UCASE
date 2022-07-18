e6fc422b004573f877b951a8cf44aa884ce2f421
DaviMoreira27 <davimsantana2706@gmail.com>
2022-07-18 10:54:29 -0300

    Finalizada a homepage, devendo apenas algumas animações

--- App/view/homepage.php
+++ App/view/homepage.php
@@ -79,10 +79,47 @@
                     <ul class="glide__slides">
                         <?php for ($i = 0; $i < 10; $i++) : ?>
 
-                            <div class="product-box">
+                            <div class="product-box glide__slide">
                                 <a href="#">
                                     <img src="../assets/./img/./Time.png" alt="imagem time do flamengas">
-                                    <h4>Flamengo - Uniforme 1<br> 2022 Personalizada</h4>
+                                    <h4>Flamengo - Uniforme 1 2022 Personalizada</h4>
+                                    <div class="stars">
+                                        <i class="fa-solid fa-star"></i>
+                                        <i class="fa-solid fa-star"></i>
+                                        <i class="fa-solid fa-star"></i>
+                                        <i class="fa-solid fa-star"></i>
+                                        <i class="fa-solid fa-star"></i>
+                                    </div>
+                                    <p>R$ 36,65</p>
+                                    <button id="principal-button">Comprar</button>
+                                </a>
+                            </div>
+
+                        <?php endfor; ?>
+                    </ul>
+                </div>
+                <div class="glide__arrows right" data-glide-el="controls">
+                    <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
+                </div>
+            </section>
+        </article>
+
+
+        <article class="product-container lancamento">
+            <h1>Lançamentos</h1>
+
+            <section class="glide product-cont prod-lanc">
+                <div class="glide__arrows left" data-glide-el="controls">
+                    <button class="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
+                </div>
+                <div class="glide__track" data-glide-el="track">
+                    <ul class="glide__slides">
+                        <?php for ($i = 0; $i < 10; $i++) : ?>
+
+                            <div class="product-box glide__slide">
+                                <a href="#">
+                                    <img src="../assets/./img/./Time.png" alt="imagem time do flamengas">
+                                    <h4>Flamengo - Uniforme 1 2022 Personalizada</h4>
                                     <div class="stars">
                                         <i class="fa-solid fa-star"></i>
                                         <i class="fa-solid fa-star"></i>
@@ -108,7 +145,8 @@
     </main>
 
 </body>
-
+<!-- Footer -->
+<?php require_once "./footer.php"; ?>
 
 
 <script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
@@ -123,7 +161,45 @@
     new Glide('.product-cont', {
         type: 'carousel',
         startAt: 0,
-        perView: 2
+        perView: 4,
+        breakpoints: {
+            1300: {
+                perView: 3
+            },
+            1100: {
+                perView: 2
+            },
+
+            550: {
+                perView: 1
+            },
+
+            350: {
+                perView: 1
+            }
+        }
+    }).mount();
+
+    new Glide('.prod-lanc', {
+        type: 'carousel',
+        startAt: 0,
+        perView: 4,
+        breakpoints: {
+            1300: {
+                perView: 3
+            },
+            1100: {
+                perView: 2
+            },
+
+            550: {
+                perView: 1
+            },
+
+            350: {
+                perView: 1
+            }
+        }
     }).mount();
 </script>
 
