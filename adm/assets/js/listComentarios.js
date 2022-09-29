d890930f070d6bfd9f92d0144ab288619bf1d186
DaviMoreira27 <davimsantana2706@gmail.com>
2022-09-28 22:33:50 -0300

    Jquery Function AJAX

--- adm/assets/js/listComentarios.js
+++ adm/assets/js/listComentarios.js
@@ -14,3 +14,25 @@ function redirectCategoria() {
           "../controller/ControllerComentarios.php?selectAvaliacao=";
     }
 }
+
+
+// $(document).ready(function () {
+//     /// Quando usuário clicar em salvar será feito todos os passo abaixo
+//     $('#selectCategoria').change(function () {
+
+//         var dados = $('#selectCategoria').serialize();
+
+//         $.ajax({
+//             type: 'POST',
+//             dataType: 'json',
+//             url: '../controller/ControllerComentarios.php?selectAvaliacao=' + opcaoTexto,
+//             async: true,
+//             data: dados,
+//             success: function (response) {
+//                 location.reload();
+//             }
+//         });
+
+//         return false;
+//     });
+// });
